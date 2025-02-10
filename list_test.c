35cf732a140133ded236b5add59bf3cef133da51
tjones221 <157931228+tjones221@users.noreply.github.com>
2025-02-10 00:34:44 -0700

    Create list_test.c
    Make a test file list test.c that tests your list implementation.  Your test file should try various combinations of your create list, add to list, remove from list, flush list, print list, and free listfunctions.  Note the difference between flush listand free list: after  calling  flush list,  your  list  ll still  points  to  a  valid,  empty  list  (like  a  brand  new list  whose  head  points  to  NULL);  after  calling  free list,  ll itself  becomes  NULL.  Note the input to free list is list **ll instead of  list *ll, otherwise setting  ll to NULL in this function will only affect the local variable in free list, not the one in main.  You should check return values from any function that can possibly fail.

---
+++ list_test.c
@@ -0,0 +1,5 @@
+//This file should test your list implementation.  Your test file should try various combinations 
+//of your create list, add to list, remove from list, flush list, print list, and free listfunctions.
+//Note the difference between flush listand free list: after  calling  flush list,  your  
+//list  ll still  points  to  a  valid,  empty  list  (like  a  brand  new list  whose  head  points  to  NULL);  
+
